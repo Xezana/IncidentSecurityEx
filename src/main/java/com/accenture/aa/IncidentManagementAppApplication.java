fac48e3c4a3c92b78c77614f228331d28b866c02
Ayassamy <krisnasamy.ayassamy@accenture.com>
2018-05-11 11:28:19 +0400

    master commit

---
+++ src/main/java/com/accenture/aa/IncidentManagementAppApplication.java
@@ -0,0 +1,12 @@
+package com.accenture.aa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncidentManagementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncidentManagementAppApplication.class, args);
+	}
+}
